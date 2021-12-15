d1bdd259921ff2d9ffdba8dde37211afa736be46
DaveSiebel <contact@davesiebel.nl>
2021-12-15 21:31:47 +0100

    Verschillende technieken geprobeerd. Nog geen bruikbaar resultaat

--- public/js/zoekfunctie.js
+++ public/js/zoekfunctie.js
@@ -4,23 +4,24 @@ let submit = document.querySelector('#submit');
 
 submit.addEventListener('click', (e) => {
 	e.preventDefault();
-	getSearch();
+	// getSearch();
+	const tab = window.open(`zoekresultaten?q=${input.value}`, '_self');
 });
 
-let getSearch = async () => {
-	let res = await axios.get(
-		`https://www.bol.com/nl/rnwy/search-suggestions/products?query=${input.value}`
-	);
-	console.log(res);
-	makeElement(res.data);
-};
+// let getSearch = async () => {
+// 	let res = await axios.get(
+// 		`https://www.bol.com/nl/rnwy/search-suggestions/products?query=${input.value}`
+// 	);
+// 	console.log(res);
+// 	makeElement(res.data);
+// };
 
-let makeElement = (data) => {
-	for (let i = 0; i < data.suggestions.length; i++) {
-		const tekst = document.createElement('P');
-		tekst.append(
-			'Product: ' + data.suggestions[i].label + ' ID:' + data.suggestions[i].id
-		);
-		document.body.append(tekst);
-	}
-};
+// let makeElement = (data) => {
+// 	for (let i = 0; i < data.suggestions.length; i++) {
+// 		const tekst = document.createElement('P');
+// 		tekst.append(
+// 			'Product: ' + data.suggestions[i].label + ' ID:' + data.suggestions[i].id
+// 		);
+// 		document.body.append(tekst);
+// 	}
+// };
