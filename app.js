67d8c6ffd31b71872f950bd54a0e2765b3ccfb15
DaveSiebel <contact@davesiebel.nl>
2021-12-18 13:52:32 +0100

    Data scraper gemaakt

--- app.js
+++ app.js
@@ -30,6 +30,38 @@ app.get('/zoekresultaten', (req, res) => {
 	res.render('zoekresultaten');
 });
 
+app.get('/product', (req, res) => {
+	res.render('product');
+});
+
+// Puppeteer
+
+const puppeteer = require('puppeteer');
+const fs = require('fs').promises;
+
+async function start() {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.goto(
+		'https://www.bol.com/nl/nl/p/apple-airpods-2-met-reguliere-oplaadcase/9200000108340791/?bltgh=omqEYt0S3RQNAvqrayNIZg.2_35.36.ProductImage'
+	);
+	await page.click('.js-confirm-button');
+	const productTitel = await page.$eval(
+		'.page-heading span',
+		(el) => el.textContent
+	);
+	const productBeschrijving = await page.$eval(
+		'.page-heading .sub-title',
+		(el) => el.textContent
+	);
+	console.log(productTitel);
+	console.log(productBeschrijving);
+	await browser.close();
+}
+
+start();
+
+// Express
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`);
 });
