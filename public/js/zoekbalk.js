cfd2de060957c670e67bbd458f15ff7c727af35e
DaveSiebel <contact@davesiebel.nl>
2021-12-20 22:57:07 +0100

    aantekeningen toevoegen

--- public/js/zoekbalk.js
+++ public/js/zoekbalk.js
@@ -1,12 +1,15 @@
+// Importeer de HTML zoekbalk en zoekknop
 let form = document.querySelector('#form');
 let input = document.querySelector('#form_input');
 let submit = document.querySelector('#submit');
 
+// Bekijk of de zoekknop of enter wordt ingedrukt
 submit.addEventListener('click', (e) => {
 	e.preventDefault();
 	getSearch(input.value);
 });
 
+// Open een nieuwe pagina waar in de link achter q= de zoekterm staat
 function getSearch(zoekQuery) {
 	window.open(`/product?q=${zoekQuery}`, '_self');
 }
